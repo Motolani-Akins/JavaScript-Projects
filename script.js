29b1e94916b0c12e814e9ac185d5f4fa382aaeb5
Motolani Akinyemi <motolaniakinyemi@gmail.com>
2022-02-05 14:18:18 +0100

    Age in Days

---
+++ script.js
@@ -0,0 +1,16 @@
+//Challenge 1: Your Age In Days
+
+function ageInDays() {
+    var birthYear = prompt ('What year were you born?');
+    var ageToDays = (2022 - birthYear) * 365;
+    console.log(ageToDays)
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageToDays + ' days old' );
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer)
+    document.getElementById('result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove(); 
+}
